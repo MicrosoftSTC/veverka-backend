1593978bd60c6f540ccfb778442fd4b7af77978c
Filip Tronicek <filip.tronicek@seznam.cz>
2020-07-24 16:24:14 +0200

    Initial Express stuff

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+const express = require('express');
+
+const app = express(); // express init
+
+const port = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('assets'));
+
+app.get('/', (_req, res) => {
+  res.status(200);
+  res.send('Initial route');
+});
+
+app.listen(port, () => {
+  console.log(`Editor listening on port ${port}!`);
+});
