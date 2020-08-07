cef1d8626c7bcdf4fd73513c92c1cad9a52b7ddf
kahy <pepa.kahy@gmail.com>
2020-08-08 01:51:32 +0200

    Epic firebase backend config

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ const app = express(); // express init
 
 const port = process.env.PORT || 5000;
 
+var admin = require("firebase-admin");
+
+// Fetch the service account key JSON file contents
+
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://veverka-back.firebaseio.com"
+});
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
+var ref = db.ref("restricted_access/secret_document");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('assets'));
 
