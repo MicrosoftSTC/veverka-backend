f6cca33a0131569575383c56e5a6820057b2b9b2
Filip Troníček <filip.tronicek@seznam.cz>
2020-08-08 01:54:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,21 +6,19 @@ const app = express(); // express init
 
 const port = process.env.PORT || 5000;
 
-var admin = require("firebase-admin");
-
 // Fetch the service account key JSON file contents
-var serviceAccount = require("service-api-key.json");
+const serviceAccount = require("service-api-key.json");
 
 // Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://veverka-back.firebaseio.com"
+  databaseURL: 'https://veverka-back.firebaseio.com'
 });
 
 // As an admin, the app has access to read and write all data, regardless of Security Rules
-var db = admin.database();
-var ref = db.ref("restricted_access/secret_document");
-ref.once("value", function(snapshot) {
+const db = admin.database();
+const ref = db.ref('restricted_access/secret_document');
+ref.once('value', (snapshot) => {
   console.log(snapshot.val());
 });
 
