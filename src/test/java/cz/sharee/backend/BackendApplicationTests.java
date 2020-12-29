a3a12387545afa3b4a649de60636bb3a9eb90c43
Jan <honzicekcahlicek1@gmail.com>
2020-12-29 20:26:45 +0100

    Biggest test of the universe

--- src/test/java/cz/sharee/backend/BackendApplicationTests.java
+++ src/test/java/cz/sharee/backend/BackendApplicationTests.java
@@ -3,11 +3,14 @@ package cz.sharee.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
-    void contextLoads() {
+    void maths() {
+        assertEquals("Ahojda", "Ahojda");
     }
 
 }
