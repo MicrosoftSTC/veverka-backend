ea4b0724222fcc853c5a97ad0f6823840307acc7
Jan <honzicekcahlicek1@gmail.com>
2021-01-15 22:42:04 +0100

    Star time

--- src/main/java/cz/sharee/backend/domain/profile/Star.java
+++ src/main/java/cz/sharee/backend/domain/profile/Star.java
@@ -1,11 +1,14 @@
 package cz.sharee.backend.domain.profile;
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 
 @Entity
 @Data
@@ -13,6 +16,10 @@ public class Star {
     @Id
     @GeneratedValue
     private Long id;
+
+    @NotNull
+    @CreationTimestamp
+    private LocalDate published;
 
     @ManyToOne
     private User starPublisher;
